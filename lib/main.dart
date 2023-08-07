b763c80a254462665a38290fbe53b7bed2abdb1b
aaronjc15128 <aaronjchauhan2@gmail.com>
2023-08-07 16:52:51 +0100

    Worked on app a bit more

--- lib/main.dart
+++ lib/main.dart
@@ -28,12 +28,32 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  Map themeColors = {
+    "Text"        : const Color(0xFFFFFFFF),
+    "Background"  : const Color(0xFF0F0F0F),
+    "Primary"     : const Color(0xFFB996F3),
+    "Secondary"   : const Color(0xFF47346B),
+    "Accent"      : const Color(0xFFF64B79),
+  };
+
   
-  // Variables & Functions go here
+  int navbarIndex = 2;
+
+  void navbarTap(int index) {
+    setState(() {
+      navbarIndex = index;
+    });
+  }
+
+
 
   List<Widget> pages() => <Widget>[
-
-    // pages go here
+    Column(),
+    Column(),
+    Column(),
+    Column(),
+    Column(),
+    
 
   ];
 
@@ -42,8 +62,8 @@ class _AppState extends State<App> {
     super.initState();
 
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.red,                         // top bar bg color
-      systemNavigationBarColor: Colors.red,               // bottom bar bg color
+      statusBarColor: Colors.transparent,                 // top bar bg color
+      systemNavigationBarColor: Colors.pinkAccent,        // bottom bar bg color
       systemNavigationBarIconBrightness: Brightness.light,  // bottom bar icon color
     ));
 
@@ -54,16 +74,68 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "title",
+      title: "Workout App",
+      debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-        /*
-        appBar: ,
-        drawer: ,
-        bottomNavigationBar: ,
-        body: pages()[],
-        */
-      ),
+        backgroundColor: themeColors["Background"],
+        
+        extendBodyBehindAppBar: true,
+        extendBody: true,
+        resizeToAvoidBottomInset: false,
+        
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(80),
+          child: AppBar(
+            elevation: 0,
+            backgroundColor: themeColors["Background"],
+            iconTheme: IconThemeData(color: themeColors["Text"]),
+            title: Text("Workouts", style: TextStyle(fontSize: 22, color: themeColors["Text"])),
+            centerTitle: true,
+          )
+        ),
+
+        bottomNavigationBar: PreferredSize(
+          preferredSize: const Size.fromHeight(80),
+          child: BottomNavigationBar(
+            elevation: 0,
+            backgroundColor: themeColors["Background"],
+            selectedItemColor: themeColors["Accent"],
+            unselectedItemColor: themeColors["Text"],
+            selectedFontSize: 16,
+            unselectedFontSize: 14,
+            iconSize: 32,
+            
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.circle),
+                label: "History",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.circle),
+                label: "Split",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.circle),
+                label: "Workouts",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.circle),
+                label: "Exercises",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.circle),
+                label: "Stats",
+              ),
+            ],
+
+            currentIndex: navbarIndex,
+            onTap: navbarTap,
+          ),
+        ),
+
+        body: pages()[navbarIndex],
+      )
     );
   }
 }
