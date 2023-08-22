d67587376841522658982211623cb08185697392
aaronjc15128 <aaronjchauhan2@gmail.com>
2023-08-22 15:39:49 +0100

    Added Donate Page

--- lib/main.dart
+++ lib/main.dart
@@ -167,6 +167,9 @@ class _AppState extends State<App> {
               ListTile(
                 leading: Icon(Icons.attach_money_rounded, color: themeColors["Text"],),
                 title: Text("Donate", style: TextStyle(color: themeColors["Text"])),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => DonatePage(themeColors: themeColors)));
+                },
               ),
               const Spacer(),
               ListTile(
@@ -226,6 +229,97 @@ class _AppState extends State<App> {
 
         body: pages()[navbarIndex],
       )
+    );
+  }
+}
+
+class DonatePage extends StatelessWidget {
+  final Map themeColors;
+  const DonatePage({Key? key, required this.themeColors}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Workout App",
+      
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: themeColors["Background"],
+          iconTheme: IconThemeData(color: themeColors["Text"]),
+          title: Text("Donate", style: TextStyle(fontSize: 22, color: themeColors["Text"])),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back_rounded)
+          ),
+        ),
+
+        body: Container(
+          color: themeColors["Background"],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 12),
+                child: Text(
+                  "As a young indie developer, who has created an app without any ads, I make no money from this app whatsoever",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16,  color: themeColors["Text"])
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 12),
+                child: Text(
+                  "I would really appreciate it if you'd donate, as little as £1, to help support the development of this app, and my others apps too",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: themeColors["Text"])
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 12),
+                child: Text(
+                  "Simply follow the link below and you can donate as little or as much as you want",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: themeColors["Text"])
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: Text(
+                  "All donations, no matter how big or small, are appreciated greatly :)",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: themeColors["Text"])
+                ),
+              ),
+              const SizedBox(height: 50),
+              Container(height: 80,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.fromLTRB(95, 0, 95, 0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: themeColors["Accent"],
+                    textStyle: const TextStyle(fontSize: 16)
+                  ),
+                  onPressed: () {
+                    Clipboard.setData(const ClipboardData(text: "https://www.flutter.dev"));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.attach_money_rounded, color: themeColors["Text"]),
+                      Text("  Donate Link", style: TextStyle(color: themeColors["Text"]))
+                    ],
+                  )
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
