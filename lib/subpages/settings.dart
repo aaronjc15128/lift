dfe35dd3848bdbce89dbb07da9b7572690c83dce
aaronjc15128 <aaronjchauhan2@gmail.com>
2023-09-05 19:50:53 +0100

    Added kg/lbs toggle in settings page

--- lib/subpages/settings.dart
+++ lib/subpages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../theme_colors.dart';
 import '../background.dart';
@@ -6,6 +7,31 @@ import '../background.dart';
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return const MaterialApp(
+      home: App(),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  String selectedUnit = "kg";
+  Color kgColor = themeColors["Box"];
+  Color lbsColor = Colors.transparent;
+
+  
   @override
   Widget build(BuildContext context){
     return MaterialApp(
@@ -41,6 +67,40 @@ class SettingsPage extends StatelessWidget {
 
                 children: <Widget>[
                   Text("Weight Unit", textAlign: TextAlign.center, style: TextStyle(color: themeColors["Text"])),
+                  const SizedBox(width: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedUnit = "kg";
+                        kgColor = themeColors["Box"];
+                        lbsColor = Colors.transparent;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      backgroundColor: kgColor,
+                      padding: const EdgeInsets.all(15),
+                      alignment: Alignment.center,
+                    ),
+                    child: Text("kg", textAlign: TextAlign.center, style: TextStyle(color: themeColors["Text"])),
+                  ),
+                  const SizedBox(width: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedUnit = "lbs";
+                        kgColor = Colors.transparent;
+                        lbsColor = themeColors["Box"];
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      backgroundColor: lbsColor,
+                      padding: const EdgeInsets.all(15),
+                      alignment: Alignment.center,
+                    ),
+                    child: Text("lbs", textAlign: TextAlign.center, style: TextStyle(color: themeColors["Text"])),
+                  ),
                 ],
               )
             ],
