665f7c7fade5098960f5188ff68b3cfc28e5cd8a
aaronjc15128 <aaronjchauhan2@gmail.com>
2023-09-11 20:24:43 +0100

    Added exercises

--- lib/mainpages/exercises.dart
+++ lib/mainpages/exercises.dart
@@ -3,16 +3,61 @@ import 'package:flutter/material.dart';
 class ExercisesPage extends StatelessWidget {
   final Map themeColors;
   const ExercisesPage({Key? key, required this.themeColors}) : super(key: key);
+
+  @override
+  void initState() {
+
+  }
   
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-  
-      children: <Widget>[
-        Text("EXERCISES Coming Soon...", textAlign: TextAlign.center, style: TextStyle(color: themeColors["Text"]))
-      ],
+    return SizedBox(
+      height: double.infinity,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+        
+          children: <Widget>[
+            const SizedBox(height: 150),
+            Text("EXERCISES Coming Soon...", textAlign: TextAlign.center, style: TextStyle(color: themeColors["Text"])),
+            const SizedBox(height: 50),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Exercise extends StatelessWidget {
+final Map themeColors;
+  const Exercise({Key? key, required this.themeColors}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        
+      },
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: Colors.transparent
+      ),
+      child: Container(height: 50,
+        margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+        padding: const EdgeInsets.all(6),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: themeColors["Box"],
+          borderRadius: const BorderRadius.all(Radius.circular(25))
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Ea fugiat est", style: TextStyle(color: themeColors["Text"])),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/storage/exercise_list.dart
@@ -0,0 +1,302 @@
+List exerciseList = [
+  {
+    'name':'Abs Wheel',
+    'type':'Bodyweight',
+    'muscle':'Abs',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Crunch',
+    'type':'Cable',
+    'muscle':'Abs',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Bent Over Row',
+    'type':'Barbell',
+    'muscle':'Back',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Shrug',
+    'type':'Dumbbell',
+    'muscle':'Back',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Lat Pulldown',
+    'type':'Machine',
+    'muscle':'Back',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Bicep Curl',
+    'type':'Barbell',
+    'muscle':'Biceps',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Hammer Curl',
+    'type':'Dumbbell',
+    'muscle':'Biceps',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Bench Press',
+    'type':'Barbell',
+    'muscle':'Chest',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Smith Bench Press',
+    'type':'Machine',
+    'muscle':'Chest',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Iso-Lateral Chest Press',
+    'type':'Dumbbell',
+    'muscle':'Chest',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Standing Reverse Wrist Curl',
+    'type':'Dumbbell',
+    'muscle':'Forearms',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Bulgarian Split Squat',
+    'type':'Barbell',
+    'muscle':'Quads',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Neck Extension',
+    'type':'Misc',
+    'muscle':'Neck',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Lateral Raise',
+    'type':'Dumbbell',
+    'muscle':'Shoulders',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Lateral Raise',
+    'type':'Cable',
+    'muscle':'Shoulders',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Tricep Extension',
+    'type':'Cable',
+    'muscle':'Triceps',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Diamond Push Up',
+    'type':'Bodyweight',
+    'muscle':'Triceps',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Tricep Extension',
+    'type':'Machine',
+    'muscle':'Triceps',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Skullcrusher',
+    'type':'Dumbbell',
+    'muscle':'Triceps',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+  {
+    'name':'Skullcrusher',
+    'type':'Barbell',
+    'muscle':'Triceps',
+
+    'prev_sets':[],
+    'prev_warmup':[],
+    'prev_drops':[],
+
+    'total_reps':0,
+    'total_volume':0,
+
+    'max_weight':0,
+    'max_onerepmax':0,
+  },
+];
