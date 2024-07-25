312c0d7073ebb688a39b7ac601511ad2bc348a3b
aaronjc15128 <aaronjchauhan2@gmail.com>
2024-07-25 15:57:11 +0100

    Basic inaworkout.dart functions

--- lib/inaworkout.dart
+++ lib/inaworkout.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../storage/workout_list.dart';
+import '../storage/preferences.dart';
 import '../theme_colors.dart';
 import '../background.dart';
-
 
 /* 
 ? not needed
@@ -39,21 +39,108 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
   void initState() {
     super.initState();
 
+    //TextEditingController controller = TextEditingController();
+
     workout = workoutList[widget.workoutIndex];
 
-    // Safely cast 'content' to a List
     List<dynamic> workoutContent = workout['content'] as List<dynamic>;
 
     setState(() {
-      widgets.add(const SizedBox(height: 130));
+      widgets.add(const SizedBox(height: 80));
       for (var i = 0; i < workoutContent.length; i++) {
         Map<String, dynamic> exercise = workoutContent[i] as Map<String, dynamic>;
         widgets.add(
           Container(
             margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+
               children: [
-                Text(exercise['name'].toString(), style: TextStyle(fontSize: 20, color: themeColors['Text'])),
+                Text(exercise['name'].toString(), style: TextStyle(fontSize: 18, color: themeColors['Text'])),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: 30,
+                      child: Text("set", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                    ),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: 130,
+                      child: Text("weight in ${preferences['weight_unit']}", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                    ),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: 130,
+                      child: Text("reps", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                    ),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: 30,
+                      child: Text("done", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                    ),
+                    const SizedBox(width: 10),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    const SizedBox(
+                      width: 30,
+                      child: Icon(Icons.add),
+                    ),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: 130,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: themeColors["LightBox"],
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: TextField(
+                          //controller: controller,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: themeColors['Text']),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: '10',
+                            hintStyle: TextStyle(color: themeColors['Background']),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: 130,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: themeColors["LightBox"],
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: TextField(
+                          //controller: controller,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: themeColors['Text']),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: '8',
+                            hintStyle: TextStyle(color: themeColors['Background']),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    const SizedBox(
+                      width: 30,
+                      child: Icon(Icons.add),
+                    ),
+                    const SizedBox(width: 10),
+                  ],
+                ),
+                const SizedBox(height: 60),
               ],
             ),
           ),
@@ -63,8 +150,6 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
     
   }
 
-
-  
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -86,10 +171,20 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
 
       body: Background(
         page: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          
-          children: widgets,
+
+          children: <Widget>[
+            SizedBox(height: 888,
+              child: SafeArea(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: widgets,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
