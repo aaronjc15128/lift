be7135408f7cf091f408880649fc0056252dfb82
aaronjc15128 <aaronjchauhan2@gmail.com>
2024-07-25 16:23:06 +0100

    Updated inaworkout.dart

--- lib/inaworkout.dart
+++ lib/inaworkout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../storage/workout_list.dart';
 import '../storage/preferences.dart';
@@ -58,7 +59,7 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
 
               children: [
                 Text(exercise['name'].toString(), style: TextStyle(fontSize: 18, color: themeColors['Text'])),
-                const SizedBox(height: 20),
+                const SizedBox(height: 10),
                 Row(
                   children: [
                     const SizedBox(width: 10),
@@ -66,17 +67,17 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
                       width: 30,
                       child: Text("set", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
                     ),
-                    const SizedBox(width: 10),
+                    const SizedBox(width: 20),
                     SizedBox(
-                      width: 130,
+                      width: 120,
                       child: Text("weight in ${preferences['weight_unit']}", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
                     ),
                     const SizedBox(width: 10),
                     SizedBox(
-                      width: 130,
+                      width: 120,
                       child: Text("reps", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
                     ),
-                    const SizedBox(width: 10),
+                    const SizedBox(width: 20),
                     SizedBox(
                       width: 30,
                       child: Text("done", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
@@ -92,9 +93,9 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
                       width: 30,
                       child: Icon(Icons.add),
                     ),
-                    const SizedBox(width: 10),
+                    const SizedBox(width: 20),
                     SizedBox(
-                      width: 130,
+                      width: 120,
                       child: Container(
                         decoration: BoxDecoration(
                           color: themeColors["LightBox"],
@@ -102,19 +103,22 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
                         ),
                         child: TextField(
                           //controller: controller,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [FilteringTextInputFormatter.digitsOnly, LengthLimitingTextInputFormatter(4)],
                           textAlign: TextAlign.center,
-                          style: TextStyle(color: themeColors['Text']),
+                          style: TextStyle(fontFamily: 'Inter', color: themeColors['Text']),
+                          cursorColor: themeColors['Text'],
                           decoration: InputDecoration(
                             border: InputBorder.none,
                             hintText: '10',
-                            hintStyle: TextStyle(color: themeColors['Background']),
+                            hintStyle: TextStyle(fontFamily: 'Inter', color: themeColors['Background']),
                           ),
                         ),
                       ),
                     ),
                     const SizedBox(width: 10),
                     SizedBox(
-                      width: 130,
+                      width: 120,
                       child: Container(
                         decoration: BoxDecoration(
                           color: themeColors["LightBox"],
@@ -122,17 +126,20 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
                         ),
                         child: TextField(
                           //controller: controller,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [FilteringTextInputFormatter.digitsOnly, LengthLimitingTextInputFormatter(3)],
                           textAlign: TextAlign.center,
-                          style: TextStyle(color: themeColors['Text']),
+                          style: TextStyle(fontFamily: 'Inter', color: themeColors['Text']),
+                          cursorColor: themeColors['Text'],
                           decoration: InputDecoration(
                             border: InputBorder.none,
                             hintText: '8',
-                            hintStyle: TextStyle(color: themeColors['Background']),
+                            hintStyle: TextStyle(fontFamily: 'Inter', color: themeColors['Background']),
                           ),
                         ),
                       ),
                     ),
-                    const SizedBox(width: 10),
+                    const SizedBox(width: 20),
                     const SizedBox(
                       width: 30,
                       child: Icon(Icons.add),
@@ -155,7 +162,7 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
     return Scaffold(
       resizeToAvoidBottomInset: false, // Prevents the background from squashing upwards
       extendBodyBehindAppBar: true,
-      
+
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -175,7 +182,7 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
           children: [
             Column(
               children: [
-                SizedBox(height: 130), // Adjust or remove this as needed
+                const SizedBox(height: 80),
                 Expanded(
                   child: SingleChildScrollView(
                     child: Column(
