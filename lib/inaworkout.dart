33251fbb7faa28d74dd30546d573a451cea38f6d
aaronjc15128 <aaronjchauhan2@gmail.com>
2024-07-26 12:16:00 +0100

    Fixed checkmarks in inaworkout.dart

--- lib/inaworkout.dart
+++ lib/inaworkout.dart
@@ -32,17 +32,12 @@ class InAWorkoutPage extends StatefulWidget {
 }
 
 class _InAWorkoutPageState extends State<InAWorkoutPage> {
-
   late Map<String, Object> workout;
-  List<Widget> widgets = <Widget>[];
-
   late List<List<bool>> done;
+
   void checkmarkToggle(int exerciseIndex, int setIndex) {
     setState(() {
       done[exerciseIndex][setIndex] = !done[exerciseIndex][setIndex];
-      print(done);
-      print(exerciseIndex);
-      print(setIndex);
     });
   }
 
@@ -62,149 +57,151 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
         done[n].add(false);
       }
     }
+  }
 
-    setState(() {
-      widgets.add(const SizedBox(height: 60));
-      for (var i = 0; i < workoutContent.length; i++) {
-        Map<String, dynamic> exercise = workoutContent[i] as Map<String, dynamic>;
+  List<Widget> buildWidgets() {
+    List<Widget> widgets = [];
+    List<dynamic> workoutContent = workout['content'] as List<dynamic>;
+
+    widgets.add(const SizedBox(height: 60));
+    for (var i = 0; i < workoutContent.length; i++) {
+      Map<String, dynamic> exercise = workoutContent[i] as Map<String, dynamic>;
+      widgets.add(
+        Container(
+          margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(exercise['name'].toString(), style: TextStyle(fontSize: 16, color: themeColors['Text'])),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <SizedBox>[
+                  const SizedBox(width: 10),
+                  SizedBox(
+                    width: 30,
+                    child: Text("set", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                  ),
+                  const SizedBox(width: 20),
+                  SizedBox(
+                    width: 120,
+                    child: Text("weight in ${preferences['weight_unit']}", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                  ),
+                  const SizedBox(width: 10),
+                  SizedBox(
+                    width: 120,
+                    child: Text("reps", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                  ),
+                  const SizedBox(width: 20),
+                  SizedBox(
+                    width: 30,
+                    child: Text("done", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+                  ),
+                  const SizedBox(width: 10),
+                ],
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      );
+
+      List<dynamic> sets = workoutContent[i]['sets'] as List<dynamic>;
+      late Color setcolor;
+      int count = 0;
+
+      for (var j = 0; j < exercise['sets'].length; j++) {
+        String set = sets[j] as String;
+        if (set == 'n') {
+          count++;
+          set = count.toString();
+          setcolor = themeColors['Primary'];
+        }
+        else if (set == 'w') {
+          setcolor = themeColors['Primary'];
+        }
+        else if (set == 'f') {
+          setcolor = themeColors['Accent'];
+        }
+
+        //Icon checkmarkIcon = checkmark(i, j);
+
         widgets.add(
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-
-              children: [
-                Text(exercise['name'].toString(), style: TextStyle(fontSize: 16, color: themeColors['Text'])),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <SizedBox>[
-                    const SizedBox(width: 10),
-                    SizedBox(
-                      width: 30,
-                      child: Text("set", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 30,
+                child: Text(set, textAlign: TextAlign.center, style: TextStyle(fontSize: 14, color: setcolor)),
+              ),
+              const SizedBox(width: 20),
+              SizedBox(
+                width: 120,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: themeColors["LightBox"],
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    //controller: controller,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly, LengthLimitingTextInputFormatter(4)],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: 'Inter', color: themeColors['Text']),
+                    cursorColor: themeColors['Text'],
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '10',
+                      hintStyle: TextStyle(fontFamily: 'Inter', color: themeColors['Background']),
                     ),
-                    const SizedBox(width: 20),
-                    SizedBox(
-                      width: 120,
-                      child: Text("weight in ${preferences['weight_unit']}", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
-                    ),
-                    const SizedBox(width: 10),
-                    SizedBox(
-                      width: 120,
-                      child: Text("reps", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
-                    ),
-                    const SizedBox(width: 20),
-                    SizedBox(
-                      width: 30,
-                      child: Text("done", textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: themeColors['Background'])),
-                    ),
-                    const SizedBox(width: 10),
-                  ],
+                  ),
                 ),
-                const SizedBox(height: 10),
-              ],
-            ),
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 120,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: themeColors["LightBox"],
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    //controller: controller,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly, LengthLimitingTextInputFormatter(3)],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: 'Inter', color: themeColors['Text']),
+                    cursorColor: themeColors['Text'],
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '8',
+                      hintStyle: TextStyle(fontFamily: 'Inter', color: themeColors['Background']),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              SizedBox(
+                width: 30,
+                child: IconButton(
+                  icon: Icon(done[i][j] ? Icons.check_circle_rounded : Icons.check_rounded),
+                  iconSize: 20,
+                  onPressed: () {
+                    checkmarkToggle(i, j);
+                  },
+                ),
+              ),
+              const SizedBox(width: 10),
+            ],
           ),
         );
-
-        List<dynamic> sets = workoutContent[i]['sets'] as List<dynamic>;
-        late Color setcolor;
-        int count = 0;
-
-        for (var j = 0; j < exercise['sets'].length; j++) {
-          String set = sets[j] as String;
-          if (set == 'n') {
-            count++;
-            set = count.toString();
-            setcolor = themeColors['Primary'];
-          }
-          else if (set == 'w') {
-            setcolor = themeColors['Primary'];
-          }
-          else if (set == 'f') {
-            setcolor = themeColors['Accent'];
-          }
-
-          //Icon checkmarkIcon = checkmark(i, j);
-
-          widgets.add(
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(width: 10),
-                SizedBox(
-                  width: 30,
-                  child: Text(set, textAlign: TextAlign.center, style: TextStyle(fontSize: 14, color: setcolor)),
-                ),
-                const SizedBox(width: 20),
-                SizedBox(
-                  width: 120,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: themeColors["LightBox"],
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: TextField(
-                      //controller: controller,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly, LengthLimitingTextInputFormatter(4)],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontFamily: 'Inter', color: themeColors['Text']),
-                      cursorColor: themeColors['Text'],
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: '10',
-                        hintStyle: TextStyle(fontFamily: 'Inter', color: themeColors['Background']),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                SizedBox(
-                  width: 120,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: themeColors["LightBox"],
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: TextField(
-                      //controller: controller,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly, LengthLimitingTextInputFormatter(3)],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontFamily: 'Inter', color: themeColors['Text']),
-                      cursorColor: themeColors['Text'],
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: '8',
-                        hintStyle: TextStyle(fontFamily: 'Inter', color: themeColors['Background']),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                SizedBox(
-                  width: 30,
-                  child: IconButton(
-                    icon: Icon(done[i][j] ? Icons.check_circle_rounded : Icons.check_rounded),
-                    iconSize: 20,
-                    onPressed: () {
-                      checkmarkToggle(i, j);
-                    },
-                  ),
-                ),
-                const SizedBox(width: 10),
-              ],
-            ),
-          );
-          widgets.add(const SizedBox(height: 10));
-        }
-        widgets.add(const SizedBox(height: 50));
+        widgets.add(const SizedBox(height: 10));
       }
-    });
-    
+      widgets.add(const SizedBox(height: 50));
+    }
+    return widgets;
   }
 
   @override
@@ -212,7 +209,6 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
     return Scaffold(
       resizeToAvoidBottomInset: false, // Prevents the background from squashing upwards
       extendBodyBehindAppBar: true,
-
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -226,7 +222,6 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
           icon: const Icon(Icons.arrow_back_rounded),
         ),
       ),
-
       body: Background(
         page: Stack(
           children: [
@@ -236,7 +231,7 @@ class _InAWorkoutPageState extends State<InAWorkoutPage> {
                 Expanded(
                   child: SingleChildScrollView(
                     child: Column(
-                      children: widgets,
+                      children: buildWidgets(),
                     ),
                   ),
                 ),
